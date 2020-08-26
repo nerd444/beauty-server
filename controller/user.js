65862d8d68af10dc8de52e938edf85be16e3fb7b
y1jay <67768207+y1jay@users.noreply.github.com>
2020-08-26 15:45:08 +0900

    add

--- controller/user.js
+++ controller/user.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 
 // @desc 회원가입
 // @route POST /api/v1/user/add
-// @ reqest email , passwd , name , phone
+// @ reqest nick_name , passwd , name , phone
 // @ response  success
 exports.createUser = async (req, res, next) => {
   let nick_name = req.body.nick_name;
@@ -14,7 +14,7 @@ exports.createUser = async (req, res, next) => {
   let phone = req.body.phone;
 
   if (!nick_name || !passwd || !name || !phone) {
-    res.status(500).json("정보를 입력하세요");
+    res.status(400).json("정보를 입력하세요");
   }
 
   const hashedPasswd = await bcrypt.hash(passwd, 8);
@@ -24,7 +24,6 @@ exports.createUser = async (req, res, next) => {
   try {
     [result] = await connection.query(query);
     user_id = result.insertId;
-    res.status(200).json({ success: true, msg: "회원가입 완료" });
   } catch (e) {
     if (e.errno == 1062) {
       // 이메일 중복된것 이다.
@@ -41,9 +40,9 @@ exports.createUser = async (req, res, next) => {
 
   try {
     [result] = await connection.query(query);
-    res.status(200).json({ success: true, msg: "토큰생성완료", token: token });
+    res.status(200).json({ success: true, msg: "회원가입 완료", token: token });
   } catch (e) {
-    res.status(400).json({ success: false, error: e });
+    res.status(500).json({ success: false, error: e });
   }
 };
 
@@ -77,7 +76,7 @@ exports.loginUser = async (req, res, next) => {
       console.log(e);
     }
   } catch (e) {
-    res.status(400).json({ success: false, error: e });
+    res.status(500).json({ success: false, error: e });
     console.log(e);
   }
 };
@@ -92,6 +91,125 @@ exports.logoutUser = async (req, res, next) => {
     [result] = await connection.query(query);
     res.status(200).json({ success: true, message: "로그아웃 완료" });
   } catch (e) {
-    res.status(400).json({ success: false, error: e });
+    res.status(500).json({ success: false, error: e });
+  }
+};
+
+//@desc  회원탈퇴
+//@route delete /api/v1/user/del
+//@requset token
+//@response  success
+exports.del = async (req, res, next) => {
+  let user_id = req.user.id;
+  let query = `delete from beauty_user where user_id = ${user_id}`;
+  try {
+    [result] = await connection.query(query);
+    res.status(200).json({ success: true });
+  } catch (e) {
+    res.status(500).json({ success: false });
+  }
+};
+
+//@desc  아이디찾기
+//@route  GET//api/v1/user/findId
+//@request  name, phone
+
+exports.findId = async (req, res, next) => {
+  let name = req.body.name;
+  let passwd = req.body.passwd;
+  let phone = req.body.phone;
+
+  let query = `select * from beauty_user where name = "${name}" and phone = "${phone}"`;
+
+  try {
+    [rows] = await connection.query(query);
+    let savedPasswd = rows[0].passwd;
+    let savedName = rows[0].name;
+    let savedPhone = rows[0].phone;
+
+    let isMatch = await bcrypt.compare(passwd, savedPasswd);
+
+    if (isMatch == false) {
+      res.status(400).json({
+        success: false,
+        message: "비밀번호가 맞지 않습니다.",
+      });
+      return;
+    } else if (savedName != name) {
+      res.status(400).json({
+        success: false,
+        message: "이름이 맞지 않습니다.",
+      });
+      return;
+    } else if (savedPhone != phone) {
+      res.status(400).json({
+        success: false,
+        message: "전화번호가 맞지 않습니다.",
+      });
+      return;
+    } else {
+      query = `select nick_name from beauty_user where name = "${name}" 
+      and phone = "${phone}" and passwd = "${savedPasswd}" `;
+      try {
+        [row] = await connection.query(query);
+        res.status(200).json({ success: true, ID: row });
+      } catch (e) {
+        res.status(400).json({ success: false });
+      }
+    }
+  } catch (e) {
+    res.status(500).json({ success: false });
+  }
+};
+
+//@desc  비밀번호 분실
+//@route  PUT/api/v1/user/findPasswd
+//@request  name, phone,nickname
+
+exports.findPasswd = async (req, res, next) => {
+  let name = req.body.name;
+  let nick_name = req.body.nick_name;
+  let phone = req.body.phone;
+  let new_passwd = req.body.new_passwd;
+  const hashedPasswd = await bcrypt.hash(new_passwd, 8);
+  let query = `select * from beauty_user where name = 
+  "${name}" and phone = "${phone}" and nick_name = "${nick_name}"`;
+
+  try {
+    [rows] = await connection.query(query);
+    let savedNick_name = rows[0].nick_name;
+    let savedName = rows[0].name;
+    let savedPhone = rows[0].phone;
+
+    if (savedNick_name != nick_name) {
+      res.status(400).json({
+        success: false,
+        message: "아이디가 맞지 않습니다.",
+      });
+      return;
+    } else if (savedName != name) {
+      res.status(400).json({
+        success: false,
+        message: "이름이 맞지 않습니다.",
+      });
+      return;
+    } else if (savedPhone != phone) {
+      res.status(400).json({
+        success: false,
+        message: "전화번호가 맞지 않습니다.",
+      });
+      return;
+    } else {
+      query = `update beauty_user set passwd = "${hashedPasswd}" where
+       name = "${name}" and phone = "${phone}" and nick_name = "${nick_name}"`;
+      try {
+        [row] = await connection.query(query);
+        res.status(200).json({ success: true });
+      } catch (e) {
+        res.status(400).json({ success: false });
+      }
+    }
+  } catch (e) {
+    res.status(500).json({ success: false, error: e });
   }
 };
