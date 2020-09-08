373ccd9409c20de13a95a58b2b225804f3d8ffa1
y1jay <67768207+y1jay@users.noreply.github.com>
2020-09-08 16:18:38 +0900

    add

--- controller/user.js
+++ controller/user.js
@@ -46,3 +46,39 @@ exports.check = async (req,res,next)=>{
         res.status(500).json({success:false})
     }
 }
+
+exports.change = async(req,res,next)=>{
+    let email = req.query.email
+    let new_nick_name = req.query.new_nick_name
+
+    let query = `update beauty_user set nick_name = "${new_nick_name}" where email = "${email}"`
+
+    try {
+        [result] = await connection.query(query)
+        res.status(200).json({ success: true});
+    } catch (e) {
+        if (e.errno == 1062) {
+            // 닉네임 중복된것 이다.
+            res
+              .status(400)
+              .json({ success: false, errno: 1 });
+            return;
+          } else {
+            res.status(500).json({ success: false});
+           
+          }
+    }
+}
+
+exports.del = async(req,res,next)=>{
+    let email = req.query.email
+
+    let query = `delete from beauty_user where email = "${email}"`
+
+    try {
+        [result] = await connection.query(query)
+        res.status(200).json({success:true})
+    } catch (e) {
+        res.status(500).json({success:false})
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  login, check
+  login, check, change, del
 } = require("../controller/user");
 
 router.route("/add").post(login);
 router.route("/check").get(check);
-
+router.route("/change").put(change)
+router.route("/del").delete(del)
 module.exports = router;
