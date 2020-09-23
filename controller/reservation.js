900f2a2962382152a384c44cc45eecda52c807e6
kim96-gitb <eoe25123@naver.com>
2020-09-23 16:01:42 +0900

    add

--- controller/reservation.js
+++ controller/reservation.js
@@ -147,7 +147,7 @@ exports.my_order_record = async (req, res, next) => {
     }
     res.status(200).json({
       success: true,
-      menu: menus,
+      menus: menus,
     });
   } catch (e) {
     res.status(400).json({ success: false, error: e });
