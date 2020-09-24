528f70a68f8d4f1b77660ddfdc3aab588a340923
nerd444 <tkdwls0712@naver.com>
2020-09-24 15:27:42 +0900

    add

--- controller/reservation.js
+++ controller/reservation.js
@@ -45,18 +45,36 @@ exports.order_record = async (req, res, next) => {
   }
 };
 
-// @desc        추가 필수 사항
+// @desc        추가 필수 사항(store)
 // @PUT         api/v1/reservation/add
 // @request     nick_name, take_out
 // @respones    success , rows
-exports.add = async (req, res, next) => {
+exports.add_store = async (req, res, next) => {
   let nick_name = req.query.nick_name;
-  let take_out = req.query.take_out;
   let people_number = req.query.people_number;
   let time = req.query.time;
 
-  let query = `update beauty_reservation set take_out = "${take_out}", 
+  let query = `update beauty_reservation set take_out = 0, 
   people_number = "${people_number}", time = "${time}" where nick_name = "${nick_name}"`;
+
+  try {
+    [rows] = await connection.query(query);
+    res.status(200).json({ success: true, rows: rows });
+  } catch (e) {
+    res.status(400).json({ success: false, error: e });
+  }
+};
+
+// @desc        추가 필수 사항(take_out)
+// @PUT         api/v1/reservation/add
+// @request     nick_name, take_out
+// @respones    success , rows
+exports.add_take_out = async (req, res, next) => {
+  let nick_name = req.query.nick_name;
+  let time = req.query.time;
+
+  let query = `update beauty_reservation set take_out = 1, 
+  time = "${time}" where nick_name = "${nick_name}"`;
 
   try {
     [rows] = await connection.query(query);
--- routes/reservation.js
+++ routes/reservation.js
@@ -7,14 +7,16 @@ const {
   order_cancle,
   cancle,
   order_total,
-  add,
   my_order_record,
+  add_take_out,
+  add_store,
 } = require("../controller/reservation");
 
 router.route("/order").post(order);
 router.route("/record").get(order_record);
 router.route("/myrecord").get(my_order_record);
-router.route("/add").put(add);
+router.route("/add_store").put(add_store);
+router.route("/add_take_out").put(add_take_out);
 router.route("/delete").delete(order_cancle);
 router.route("/cancle").delete(cancle);
 router.route("/").get(order_total);
